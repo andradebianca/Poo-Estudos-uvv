2f945012174832b09123a3c8710b4165de498253
Bianca M Andrade <biancamattaandrade@gmail.com>
2026-03-16 16:01:19 -0300

    JOptionPane.
    Atualização de erros.

--- BancoSistema/src/main/java/com/bianca/poo/bancosistema/Principal.java
+++ BancoSistema/src/main/java/com/bianca/poo/bancosistema/Principal.java
@@ -1,24 +1,12 @@
 package com.bianca.poo.bancosistema;
-
-<<<<<<< Updated upstream
-=======
 import javax.swing.JOptionPane;
 // Biblioteca de telas gráficas. Tendo duas mensagerias: apresentação e capturação de digitação de usuários.
->>>>>>> Stashed changes
+
 
 public class Principal {
 
     public static void main(String[] args) {
 
-<<<<<<< Updated upstream
-    Conta c1 = new Conta(11, 202, 30);
-    Conta c2 = new Conta(22, 202, 0);
-    
-    c2.creditar(500);
-    c2.transferir(c1, 100);
-    
-        System.out.println(c1.imprimir() + "\n---------\n" + c2.imprimir());
-=======
         Conta[] conta = new Conta[3];
 // Cria um vetor (array) chamado conta com 3 posições para armazenar objetos da classe Conta.
 
@@ -39,6 +27,5 @@ public class Principal {
 
         }
 
->>>>>>> Stashed changes
     }
 }
